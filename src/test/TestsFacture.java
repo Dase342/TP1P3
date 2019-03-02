93d5eb228aba96c931bed0fc42fa9bada02b5674
Pierre-Rafael Vellone <killer_paul_10@hotmail.com>
2019-03-02 18:14:52 -0500

    test/TestsFacture: test de CalculeTaxes Signed-off-by: Pierre-Rafael Vellone

--- src/test/TestsFacture.java
+++ src/test/TestsFacture.java
@@ -4,22 +4,27 @@ import org.mockito.Mock;
 import org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class TestsFacture {
 
 	@Mock
-	private gererCommande;
-	private list<String> listString;
+	private GererCommande gererCommande;
 	
+	@Before
+	void setUp() {
+		
+	}
+	@After
+	void tearDown(){
+		
+	}
 	@Test
 	void testCalculeTaxes() {
 		double testDix = Facture.calculeTaxes(10);
 		assertEquals(11.5, testDix,0.01);
-	}
-	@Test
-	void testCalculeFacture() {
-		fail( "Not yet implemented" );
 	}
 
 }
