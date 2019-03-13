51b758f6668100f8ab2d10fca4580221919e5629
Pierre-Rafael Vellone <pierre-rafael@hotmail.com>
2019-03-13 10:19:06 -0400

    GererFacture: ajout de creerPlat Signed-off-by: Pierre-Rafael Vellone

--- src/tp/GererFacture.java
+++ src/tp/GererFacture.java
@@ -1,7 +1,7 @@
 package tp;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class GererFacture {
 	private TextFile fichierText;
@@ -9,50 +9,83 @@ public class GererFacture {
 	private ArrayList<Plat> listPlat;
 	private ArrayList<Commande> listCommande;
 	private ArrayList<String> listErreur;
-	
-	public GererFacture(TextFile texte) {
-		newText(texte);
-		listClient =  new ArrayList<Client>();
+
+	public GererFacture( TextFile texte ) {
+		newText( texte );
+		listClient = new ArrayList<Client>();
 		listPlat = new ArrayList<Plat>();
 		listCommande = new ArrayList<Commande>();
-		listErreur = new  ArrayList<String>();
-		
+		listErreur = new ArrayList<String>();
+
 	}
-	public void newText(TextFile texte){
+
+	public void newText( TextFile texte ) {
 		this.fichierText = texte;
 	}
+
 	public void creerClient() {
 		String[] text = fichierText.getText();
-		List<Client> client = new ArrayList<Client>();
+		ArrayList<Client> client = new ArrayList<Client>();
 		boolean dansClient = false;
-		
+
 		for ( int i = 0; i < text.length; i++ ) {
 			if ( text[i].equals( "Clients:" ) ) {
 				dansClient = true;
 			}
 			if ( text[i].equals( "Plats:" ) ) {
 				dansClient = false;
-			} 
+			}
 			if ( dansClient ) {
 				client.add( new Client( text[i] ) );
 			}
 		}
-		if(!client.isEmpty()) {
+		if ( !client.isEmpty() ) {
 			client = listClient;
 		} else {
-			listErreur.add( "Erreur Creation de Client : Aucun Client Detecter.");
+			listErreur.add( "Erreur! Creation de Client : Aucun Client Detecter." );
 		}
 	}
-	public ArrayList<Client> getClient(){
+	
+	public void creerPlat() {
+		String[] text = fichierText.getText();
+		ArrayList<Plat> plats = new ArrayList<Plat>();
+		boolean dansPlats = false;
+		String[] ligne = null;
+		
+		for ( int i = 0; i < text.length; i++ ) {
+			if ( text[i].equals( "Plats:" ) ) {
+				dansPlats = true;
+			} else if ( text[i].equals( "Commandes:" ) ) {
+				dansPlats = false;
+			} else if ( dansPlats ) {
+				try {
+					ligne = text[i].split( " " );
+					plats.add( new Plat( ligne[0], Double.parseDouble( ligne[1] ) ) );
+				} catch (Exception e) {
+					listErreur.add( "Erreur! Creation de Plat : Plat mal formaté -> " + ligne );
+				}
+				
+			}
+		}
+		if(!plats.isEmpty()) {
+			listPlat = plats;
+		} else {
+			listErreur.add( "Erreur! Creation de Plat : Aucun Plat Detecter." );
+		}
+	}
+	public ArrayList<Client> getClient() {
 		return listClient;
 	}
-	public ArrayList<Plat> getPlat(){
+
+	public ArrayList<Plat> getPlat() {
 		return listPlat;
 	}
-	public ArrayList<Commande> getCommande(){
+
+	public ArrayList<Commande> getCommande() {
 		return listCommande;
 	}
-	public ArrayList<String> getErreur(){
+
+	public ArrayList<String> getErreur() {
 		return listErreur;
 	}
 }
