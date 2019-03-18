c8389bb8037dda5dfa7e66795e0c6c8488b2cad1
Pierre-Rafael Vellone <pierre-rafael@hotmail.com>
2019-03-18 13:31:45 -0400

    changement sur les import de TestsFacture Signed-off-by: Pierre-Rafael Vellone

--- src/test/TestsFacture.java
+++ src/test/TestsFacture.java
@@ -7,7 +7,7 @@ import tp.TextFile;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
