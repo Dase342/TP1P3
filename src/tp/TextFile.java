318753f9ea7d4bef678aa7aada23c2f6fff5d09b
Pierre-Rafael Vellone <pierre-rafael@hotmail.com>
2019-03-18 13:29:27 -0400

    TextFile: Ajjour de constructeur String[] et methode save Signed-off-by: Pierre-Rafael Vellone

--- src/tp/TextFile.java
+++ src/tp/TextFile.java
@@ -1,7 +1,9 @@
 package tp;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -17,6 +19,26 @@ public class TextFile{
 			  
 		} catch (IOException e) {
 			System.out.println( "Introuvable : " + path );
+		}
+	}
+	
+	public TextFile(String [] text) {
+		this.file = text;
+	}
+	
+	public void save(String path) {
+		try {
+			String text = "";
+			for(String line: file) {
+				text+=line;
+				text+="\r\n";
+			}
+			PrintWriter out = new PrintWriter(path);
+			
+			out.println(text);
+			out.close();
+		} catch ( FileNotFoundException e ) {
+			System.out.println( "TextFile save Error" );
 		}
 	}
 	
