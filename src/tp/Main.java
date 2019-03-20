b943098af8f35e899e265e420d24d1ca8076487f
Pierre-Rafael Vellone <pierre-rafael@hotmail.com>
2019-03-20 10:38:13 -0400

    Main: Fonctionelle Signed-off-by: Pierre-Rafael Vellone

--- src/tp/Main.java
+++ src/tp/Main.java
@@ -1,6 +1,5 @@
 package tp;
 
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -8,41 +7,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<Plat>  listPlat;
-		List<Client> listClient;
-		List<Commande> listCommande;
 		
 		System.out.print("Entrer le nom du fichier: ");
 		Scanner scanner = new Scanner(System.in);
 		String fic = scanner.nextLine();
 		scanner.close();
 		
-		TextFile fichier = new TextFile(System.getProperty("user.dir")+"\\" + fic);
-		
-		try {
-			listClient = fichier.getClient();
-			listPlat = fichier.getPlat();
-			listCommande = fichier.getCommandes( listClient, listPlat );
-			
-			facture(listClient, listCommande);
-			
-		} catch ( Exception e ) {
-			System.out.println( "Le fichier ne respecte pas le format demandé !" );
-		}
-
+		GererCommande gererCommande = new GererCommande(new TextFile(System.getProperty("user.dir")+"\\" + fic));
+		Facture facture = new Facture(gererCommande);
+		facture.enregistrerFacture();
 	}
-	
-	public static void facture(List<Client> listClient,List<Commande> listCommande) {
-		for(Commande commande: listCommande) {
-			commande.getClient().addTotal( commande.getTotale() );
-		}
-		System.out.println( "Bienvenue chez Barette!\nFactures: ");
-		for(Client client: listClient) {
-			System.out.println( client );
-		}
-		
-	}
-	
-	
 
 }
